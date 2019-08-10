b216e89050a322c856674ea477b0b11c6ae96f66
aaronshivers <aaron@moonmail.org>
2019-08-10 08:16:34 -0500

    setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import ErrorPage from './pages/ErrorPage'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route path="/" exact component={ Home } />
+      <Route path="/rooms" exact component={ Rooms } />
+      <Route path="/rooms/:id" component={ SingleRoom } />
+      <Route component={ ErrorPage } />
+    </Switch>
+  )
 }
 
-export default App;
+export default App
